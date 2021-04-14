eb7ee1028550f2ec473ec25f04e5fa8a55f39c76
Sreekar Shesham <sreekar.shesham@monitrahealth.com>
2021-04-14 13:30:04 +0530

    conditional rendreing

---
+++ condtional-rendering/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2 v-if="num === 0">The number is 0</h2>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+
+    return{
+
+      num:0,
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
